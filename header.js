cf2d178ebc555d8d76cdc52ab3d4a1acb744c62d
kboysm <lishal12@naver.com>
2020-05-23 21:11:56 +0900

    navBar 뼈대작업

---
+++ header.js
@@ -0,0 +1,10 @@
+function searchBarFoucus() {
+    let testtest = document.getElementsByClassName("search_box")[0].style.display = "block";
+    console.log(testtest)
+}
+function searchBarBlur() {
+    document.getElementsByClassName("search_box")[0].style.display = "none";
+}
+function didNotDevelop() {
+    alert("이 부분은 구현하지 않습니다.")
+}
