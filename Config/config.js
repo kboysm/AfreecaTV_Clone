6b04d09967644cb18b04971f5d23afe4cbfb58c6
kboysm <lishal12@naver.com>
2020-06-16 03:44:06 +0900

    User_Schema

---
+++ Config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    recaptchaSecretKey: '',
+    dbUrl: 'mongodb://localhost:27017/praMongo',
+    // dbUrl: 'mongodb://localhost:27017/nemv',
+    admin: {
+        id: 'LSM',
+        pwd: '1234',
+        name: '관리자'
+    }
+}
