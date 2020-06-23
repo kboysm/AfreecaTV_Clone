af3de6e9136af69c2f01f9555eb28924c6ab676e
kboysm <lishal12@naver.com>
2020-06-23 19:52:19 +0900

    product 모델 설계 및 가데이터 테스트 완료

--- back/routes/index.js
+++ back/routes/index.js
@@ -4,6 +4,7 @@ var createError = require("http-errors"); //상단에 추가
 router.use("/api/register", require("./api/register"));
 router.use("/api/notice", require("./api/notice"));
 router.use("/api/sign", require("./api/sign"));
+router.use("/api/product", require("./api/product"));
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
